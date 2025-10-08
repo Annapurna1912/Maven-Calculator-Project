c7111273af9fe27b531dc7b46ee221960ff98700
Annapurna singh <annapurna.arden@gmail.com>
2025-10-08 20:42:22 +0530

    Initial commit - Maven calculator project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(15, calc.add(10, 5));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(5, calc.subtract(10, 5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(50, calc.multiply(10, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.0, calc.divide(10, 5), 0.0);
+    }
+}
